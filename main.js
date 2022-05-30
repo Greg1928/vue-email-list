d51db63787790ba3b149cc393847b8b958105386
Greg1928 <gregoriosortino@protonmail.com>
2022-05-30 16:23:20 +0200

    added 10 emails list

---
+++ main.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        emails: []
+    },
+    created(){
+        for(let i = 0; i<10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((res) => {
+                this.emails.push(res.data.response);
+            });
+        }
+        console.log(this.emails);
+    }
+})
